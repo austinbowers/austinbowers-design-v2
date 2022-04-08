1223ad6d893c95f17c6100d64ff23e0db330aea4
Austin Bowers <austin@solarreviews.com>
2022-04-08 10:52:42 -0500

    Update vite config.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://austinbowers.design/austinbowers-design-v2/',
+  base: '/austinbowers-design-v2/',
   plugins: [vue()]
 })
