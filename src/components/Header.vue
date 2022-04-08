f478160f30cd73889ebdaa85066f5d63c50f015b
Austin Bowers <austin@solarreviews.com>
2022-04-08 12:08:56 -0500

    Update headeer.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-between fixed w-full z-50 items-center h-16 px-8">
+    <div class="flex justify-between dark:bg-gray-900 bg-white w-full z-50 items-center h-16 px-8">
         <div></div>
         <ThemeToggler/>
     </div>
