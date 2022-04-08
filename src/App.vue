74ddf7ec26f8603ae08e79cc3ff0ab36d8c46a52
Austin Bowers <austin@solarreviews.com>
2022-04-08 10:28:21 -0500

    Add Initial Files.

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header/>
+  <Landing/>
+</template>
+
+<script>
+    import Header from './components/Header.vue'
+    import Landing from './components/Landing.vue'
+
+    export default {
+        components: {
+            Header, 
+            Landing
+        }
+    }
+</script>
+
+<style lang="scss">
+    body {
+      @apply transition duration-300;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex justify-between items-center h-16 bg-white dark:bg-gray-900 px-8">
+        <div></div>
+        <ThemeToggler/>
+    </div>
+</template>
+
+<script>
+import ThemeToggler from '../components/ThemeToggler.vue'
+
+export default {
+    components: {
+        ThemeToggler
+    }
+}
+</script>
+
+<style>
+
+</style>
